f6ea37ad1cf7f1ff74cb16379b31ead2b200fa78
Sambhawi Raj Nandini <119286849+Sambhawii@users.noreply.github.com>
2023-04-01 19:29:04 +0530

    Add files via upload

---
+++ Array/ToplitzMatrix.java
@@ -0,0 +1,22 @@
+package Array2D;
+
+public class ToplitzMatrix {
+    public static void main(String[] args) {
+        int arr[][] = {
+                { 1, 2, 3, 4 },
+                { 5, 1, 2, 3 },
+                { 4, 5, 1, 2 }
+        };
+        int row = arr.length;
+        int col = arr[0].length;
+        outer: for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (arr[i][j] != arr[i + 1][j + 1]) {
+                    System.out.println("Not Toeplitz...");
+                    break outer;
+                }
+            }
+
+        }
+    }
+}
